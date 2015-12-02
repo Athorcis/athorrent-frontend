e1a2ff6e81a5d6916bdd941e06770951783d1e0c
Athorcis <athorcis@yahoo.fr>
2015-12-02 11:44:29 +0100

    locale is not set anymore using preferred language

--- app/translation.php
+++ app/translation.php
@@ -2,11 +2,6 @@
 
 use Jenyak\I18nRouting\Provider\I18nRoutingServiceProvider;
 use Silex\Application;
-use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\Translator;
 
@@ -17,22 +12,8 @@ function initializeTranslation(Application $app) {
     $app->register(new I18nRoutingServiceProvider());
     $app['i18n_routing.locales'] = $app['locales'];
 
-    $app['dispatcher']->addListener(KernelEvents::REQUEST, function (GetResponseEvent $event) {
-        $request = $event->getRequest();
-
-        if ($request->query->has('locale')) {
-            $locale = $request->query->get('locale');
-        } else if ($request->cookies->has('locale')) {
-            $locale = $request->cookies->get('locale');
-        } else {
-            $locale = $request->getPreferredLanguage();
-        }
-
-        $request->setLocale($locale);
-    }, Application::EARLY_EVENT);
-
     $app->register(new Silex\Provider\TranslationServiceProvider(), array (
-        'locale_fallbacks' => array('fr'),
+        'locale_fallbacks' => array($app['locale']),
     ));
 
     $app['translator'] = $app->share($app->extend('translator', function(Translator $translator, $app) {
@@ -47,5 +28,3 @@ function initializeTranslation(Application $app) {
 
     $app['translator.cache_dir'] = CACHE . DIRECTORY_SEPARATOR . 'translator';
 }
-
-?>
