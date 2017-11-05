dc0db09629de6980d46ba4b4eb0feff7871a3b27
Athorcis <athorcis@athorcis.ovh>
2017-11-05 14:20:44 +0100

    only register UserRole type when used

--- src/Application/BaseApplication.php
+++ src/Application/BaseApplication.php
@@ -42,9 +42,8 @@ class BaseApplication extends Application
             ]
         ]);
 
-        Type::addType('UserRole', 'Athorrent\Database\Type\UserRole');
-
         $this['orm.repo.user'] = function (Application $app) {
+            Type::addType('UserRole', 'Athorrent\Database\Type\UserRole');
             return $app['orm.em']->getRepository('Athorrent\\Database\\Entity\\User');
         };
 
