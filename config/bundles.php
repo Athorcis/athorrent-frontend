9419a628ae94c40f3da88b274ac14a83d9e43b5f
Athorcis <athorcis@athorcis.ovh>
2018-06-02 18:01:30 +0200

    add monolog

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Phpfastcache\Bundle\PhpfastcacheBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true]
 ];
