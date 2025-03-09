381d08368d79690a9aa5dff65275ce64aa5e4759
Athorcis <Athorcis@users.noreply.github.com>
2025-03-09 10:05:30 +0100

    fix: fix using second level cache on UserHasRole relationship

--- src/Database/Entity/UserHasRole.php
+++ src/Database/Entity/UserHasRole.php
@@ -4,13 +4,16 @@ namespace Athorrent\Database\Entity;
 
 use Athorrent\Database\Type\UserRole;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Cache;
 
 #[ORM\Entity]
+#[Cache(usage: 'READ_ONLY')]
 class UserHasRole
 {
     #[ORM\Id]
     #[ORM\ManyToOne(targetEntity: 'User', inversedBy: 'hasRoles')]
     #[ORM\JoinColumn(onDelete: 'CASCADE')]
+    #[Cache(usage: 'NONSTRICT_READ_WRITE')]
     private User $user;
 
     #[ORM\Id]
