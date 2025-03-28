45bd525f86686de10c4856282eaa115a303a19e1
Athorcis <Athorcis@users.noreply.github.com>
2025-03-28 19:21:40 +0100

    fix: fix deprecation error

--- src/Backend/BackendUnavailableException.php
+++ src/Backend/BackendUnavailableException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class BackendUnavailableException extends Exception
 {
-    public function __construct(readonly BackendState $state, Throwable $previous = null)
+    public function __construct(readonly BackendState $state, ?Throwable $previous = null)
     {
         parent::__construct(sprintf('backend is unavailable [state=%s]', $this->state->value), 0, $previous);
     }
