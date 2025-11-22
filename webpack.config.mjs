b7b472ca2232b76dc88cb7d6c43f4bdfc32b80f6
Athorcis <Athorcis@users.noreply.github.com>
2025-11-22 10:51:59 +0100

    fix: fix generated js to only support current browsers

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -78,12 +78,6 @@ Encore
     //     config.plugins.push('@babel/a-babel-plugin');
     // })
 
-    // enables and configure @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
-    })
-
     // enables Sass/SCSS support
     .enableSassLoader(function (options) {
         options.sassOptions.quietDeps = true;
