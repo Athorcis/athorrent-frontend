058e9c2ebf9e621a37b61177d3304789daad0d66
Athorcis <Athorcis@users.noreply.github.com>
2025-12-31 08:02:00 +0100

    chore: upgrade test deps

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   e2e: {
       baseUrl: 'https://athorrent.local',
-      experimentalStudio: true,
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
