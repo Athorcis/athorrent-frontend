cd2dbc721fef6d5bd6d546682e88a4994c0e1fe7
Athorcis <Athorcis@users.noreply.github.com>
2026-06-12 08:46:34 +0200

    fix: enabling WAL on sqlite

---
+++ src/Database/SqlitePragmaDriver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Athorrent\Database;
+
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Driver\Middleware\AbstractDriverMiddleware;
+
+class SqlitePragmaDriver extends AbstractDriverMiddleware
+{
+    public function connect(array $params): Connection
+    {
+        $connection = parent::connect($params);
+
+        $driverName = $params['driver'] ?? '';
+
+        if ($driverName === 'pdo_sqlite' || str_contains($driverName, 'sqlite')) {
+            $connection->exec('PRAGMA foreign_keys = ON;');
+
+            $connection->exec('PRAGMA journal_mode=WAL;');
+            $connection->exec('PRAGMA synchronous=NORMAL;');
+            //$connection->exec('PRAGMA busy_timeout=5000;');
+        }
+
+        return $connection;
+    }
+}
---
+++ src/Database/SqlitePragmaMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Athorrent\Database;
+
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsMiddleware;
+use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Driver\Middleware;
+
+#[AsMiddleware(connections: ['default'])]
+class SqlitePragmaMiddleware implements Middleware
+{
+    public function wrap(Driver $driver): Driver
+    {
+        return new SqlitePragmaDriver($driver);
+    }
+}
