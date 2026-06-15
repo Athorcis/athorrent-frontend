af25ee90d067afea13164f086b93731c3cdade53
Athorcis <Athorcis@users.noreply.github.com>
2026-06-15 19:00:11 +0200

    fix: trying to run tests from built docker images

--- docker/scripts/up-env.sh
+++ docker/scripts/up-env.sh
@@ -4,6 +4,7 @@ set -eu
 
 upEnv() {
     local env=$1
+    local addon=$2
     local scriptDir
     scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
 
@@ -12,16 +13,33 @@ upEnv() {
 
     if [ ! -f "$composeFilePath" ]
     then
-        echo "env $env is not supported"
+        echo "env $env is not supported" >&2
         exit 1
     fi
 
-    if [ "$env" = test ]
+    local composeArgs=(-f "$composeFilePath")
+
+    if [ -n "$addon" ]
     then
-        docker compose -f "$composeFilePath" run --rm --build -e APP_INIT=true php php bin/console tests:data:reset -v
+        local addonPath=$(dirname -- "$scriptDir")/deployment/base.$addon.yml
+
+        if [ ! -f  "$addonPath" ]
+        then
+            echo "addon $addon is not supported" >&2
+            exit 1
+        fi
+
+        composeArgs+=("-f" "$addonPath")
     fi
 
-    docker compose -f "$composeFilePath" up --pull always -d --build
+    local buildArgs=(--build --pull always)
+
+    if [ "$env" = test ]
+    then
+        docker compose "${composeArgs[@]}" run --rm "${buildArgs[@]}" -e APP_INIT=true php php bin/console tests:data:reset -v
+    fi
+
+    docker compose "${composeArgs[@]}" up "${buildArgs[@]}" -d
 }
 
 upEnv "$@"
