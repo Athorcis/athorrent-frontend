1695fef81dee7e9f07178c4c46a3a01bdee17a0b
Athorcis <Athorcis@users.noreply.github.com>
2026-07-19 07:09:26 +0200

    chore: drop core.js & tighten browserlist

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,18 +1,7 @@
-import corejsPackage from "core-js/package.json" with { type: "json" };
 
 export default {
-    targets: "defaults",
     presets: [
         "@babel/preset-env",
         "@babel/preset-typescript",
-    ],
-    plugins: [
-        [
-            "babel-plugin-polyfill-corejs3",
-            {
-                method: "usage-global",
-                version: corejsPackage.version,
-            },
-        ],
     ],
 };
